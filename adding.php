03cdbd83c235969ca626896d75ef8a5d207c578f
saranya <112763017+Saran-juno@users.noreply.github.com>
2023-01-29 12:48:40 +0530

    sample php program

---
+++ adding.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        $txt="Hello World";
+        $x=5;
+        $y=15;
+        
+        echo $txt;
+        echo"<br>";
+        echo $x;
+        echo"<br>";
+        echo $y;
+        ?>
+    </body>
+</html>
